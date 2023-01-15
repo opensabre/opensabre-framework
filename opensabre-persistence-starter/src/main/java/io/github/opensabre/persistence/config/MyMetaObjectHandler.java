0143967bb6a06ab50f5f4bf7cc84a0fb82e94072
周涛 <zhoutaoo@foxmail.com>
2023-01-15 12:45:55 +0800

    persistence mybatis fix

--- opensabre-persistence-starter/src/main/java/io/github/opensabre/persistence/config/MyMetaObjectHandler.java
+++
@@ -1,9 +0,0 @@
-package io.github.opensabre.persistence.config;
-
-import io.github.opensabre.persistence.handler.PoMetaObjectHandler;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-
-@AutoConfiguration
-public class MyMetaObjectHandler extends PoMetaObjectHandler {
-
-}
--- opensabre-persistence-starter/src/main/java/io/github/opensabre/persistence/config/MybatisConfig.java
+++ opensabre-persistence-starter/src/main/java/io/github/opensabre/persistence/config/MybatisConfig.java
@@ -1,8 +1,11 @@
 package io.github.opensabre.persistence.config;
 
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import io.github.opensabre.persistence.handler.PoMetaObjectHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,18 +15,25 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class MybatisConfig {
     /**
-     * 初使化Mybatis审计字段自动赋值的interceptor
+     * 初使化Mybatis审计字段自动赋值
      */
     @Bean
-    public ISqlInjector sqlInjector() {
-        return new LogicSqlInjector();
+    public PoMetaObjectHandler poMetaObjectHandler() {
+        return new PoMetaObjectHandler();
     }
 
     /**
-     * 分页插件
+     * mybatis插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //防止全表更新与删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        //sql 性能规范插件
+        interceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
+        return interceptor;
     }
 }
