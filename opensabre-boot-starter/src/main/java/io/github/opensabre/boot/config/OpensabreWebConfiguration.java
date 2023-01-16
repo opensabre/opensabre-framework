bd048eb5e93b715c19e973d305e8e604423b76de
周涛 <zhoutaoo@foxmail.com>
2023-01-16 22:15:54 +0800

    优化统一响应封装，Rest返回String类型使用Result进行封装返回，不进行统一响应自动包装

---
+++ opensabre-boot-starter/src/main/java/io/github/opensabre/boot/config/OpensabreWebConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.opensabre.boot.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web配置
+ */
+@AutoConfiguration
+public class OpensabreWebConfiguration implements WebMvcConfigurer {
+
+
+}
