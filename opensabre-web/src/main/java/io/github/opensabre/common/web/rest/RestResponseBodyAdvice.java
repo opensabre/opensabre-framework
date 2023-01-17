93bb126eaf9c497bd86d6f74b4f101c238573dc9
周涛 <zhoutaoo@foxmail.com>
2023-01-17 23:24:14 +0800

    fix RestResponseBodyAdvice with string

--- opensabre-web/src/main/java/io/github/opensabre/common/web/rest/RestResponseBodyAdvice.java
+++ opensabre-web/src/main/java/io/github/opensabre/common/web/rest/RestResponseBodyAdvice.java
@@ -1,6 +1,8 @@
 package io.github.opensabre.common.web.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.opensabre.common.core.entity.vo.Result;
+import lombok.SneakyThrows;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.http.MediaType;
@@ -8,6 +10,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -25,13 +28,12 @@ public class RestResponseBodyAdvice implements ResponseBodyAdvice<Object> {
         // 如果不需要进行封装的，可以添加一些校验手段，比如添加标记排除的注解
         boolean hasResponseBody = AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), ResponseBody.class) ||
                 returnType.hasMethodAnnotation(ResponseBody.class);
-        // String类型不做处理，Rest方法返回String使用Result进行包装返回
-        if (returnType.getGenericParameterType().equals(String.class)) {
-            return false;
-        }
-        return hasResponseBody;
+        boolean hasRestController = AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), RestController.class) ||
+                returnType.hasMethodAnnotation(RestController.class);
+        return hasResponseBody && hasRestController;
     }
 
+    @SneakyThrows
     @Override
     public Object beforeBodyWrite(Object body,
                                   @NotNull MethodParameter returnType,
@@ -42,6 +44,10 @@ public class RestResponseBodyAdvice implements ResponseBodyAdvice<Object> {
         // 提供一定的灵活度，如果body已经被包装了，就不进行包装
         if (body instanceof Result) {
             return body;
+        }
+        // 如果是String，将Result转为json string
+        if (body instanceof String) {
+            return new ObjectMapper().writeValueAsString(Result.success(body));
         }
         return Result.success(body);
     }
