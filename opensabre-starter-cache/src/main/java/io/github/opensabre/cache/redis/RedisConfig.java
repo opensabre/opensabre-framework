721e2a5f20b6f7645aaeafc6bec2b1a92a4446e6
周涛 <zhoutaoo@foxmail.com>
2023-03-29 22:21:43 +0800

    重构，保留jetcache，去掉cache redis

--- opensabre-starter-cache/src/main/java/io/github/opensabre/cache/redis/RedisConfig.java
+++ opensabre-starter-cache/src/main/java/io/github/opensabre/cache/redis/RedisConfig.java
@@ -1,55 +1,10 @@
 package io.github.opensabre.cache.redis;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-import java.time.Duration;
-
-@EnableCaching
 @AutoConfiguration
+@EnableMethodCache(basePackages = "io.github.opensabre")
 public class RedisConfig extends CachingConfigurerSupport {
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        //对象的序列化
-        RedisSerializationContext.SerializationPair<Object> valueSerializationPair
-                = RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer());
-        //全局redis缓存过期时间
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofDays(1))
-//                .serializeKeysWith()
-                .serializeValuesWith(valueSerializationPair);
-
-        return new RedisCacheManager(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory), redisCacheConfiguration);
-    }
-
-    private Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer() {
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper());
-        return jackson2JsonRedisSerializer;
-    }
-
-    private ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.activateDefaultTyping(
-                LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL,
-                JsonTypeInfo.As.WRAPPER_ARRAY);
-        return objectMapper;
-    }
 }
