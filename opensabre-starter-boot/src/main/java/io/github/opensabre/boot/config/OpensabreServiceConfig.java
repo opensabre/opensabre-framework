401d7396ae5ded5cc270146a30861acdce5b5deb
周涛 <zhoutaoo@foxmail.com>
2023-05-31 23:29:44 +0800

    ServiceConfig 改为@Import初使化

--- opensabre-starter-boot/src/main/java/io/github/opensabre/boot/config/OpensabreServiceConfig.java
+++ opensabre-starter-boot/src/main/java/io/github/opensabre/boot/config/OpensabreServiceConfig.java
@@ -4,18 +4,9 @@ package io.github.opensabre.boot.config;
 import io.github.opensabre.boot.event.OpensabreStartedEventHandler;
 import io.github.opensabre.boot.rest.MappingInfoHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
+@Import({OpensabreStartedEventHandler.class, MappingInfoHandler.class})
 public class OpensabreServiceConfig {
-
-    @Bean
-    public OpensabreStartedEventHandler opensabreStartedEventHandler() {
-        return new OpensabreStartedEventHandler();
-    }
-
-    @Bean
-    public MappingInfoHandler mappingInfoHandler() {
-        return new MappingInfoHandler();
-    }
 }
