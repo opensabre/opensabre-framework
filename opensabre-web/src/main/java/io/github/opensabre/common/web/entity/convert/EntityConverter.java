ddded1221c1cce5d71be3f3a92681ddc36b4798b
周涛 <zhoutaoo@foxmail.com>
2023-06-06 23:20:08 +0800

    fix doc deploy fail

--- opensabre-web/src/main/java/io/github/opensabre/common/web/entity/convert/EntityConverter.java
+++ opensabre-web/src/main/java/io/github/opensabre/common/web/entity/convert/EntityConverter.java
@@ -2,8 +2,8 @@ package io.github.opensabre.common.web.entity.convert;
 
 /**
  * 对象转换
- * Form -> Po
- * QueryForm -> Param
+ * Form -》 Po
+ * QueryForm -》 Param
  */
 public interface EntityConverter {
     <F, P> P convert(F source, Class<P> entityClass);
