8c7eb462c973065c7984b5ba08cbc540dce7ec9d
周涛 <zhoutaoo@foxmail.com>
2023-06-15 22:43:36 +0800

    去除@EnableFeignClients注解

--- opensabre-starter-rpc/src/main/java/io/github/opensabre/rpc/openfeign/config/OpensabreFeignConfig.java
+++ opensabre-starter-rpc/src/main/java/io/github/opensabre/rpc/openfeign/config/OpensabreFeignConfig.java
@@ -1,9 +1,7 @@
 package io.github.opensabre.rpc.openfeign.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @AutoConfiguration
-@EnableFeignClients
 public class OpensabreFeignConfig {
 }
