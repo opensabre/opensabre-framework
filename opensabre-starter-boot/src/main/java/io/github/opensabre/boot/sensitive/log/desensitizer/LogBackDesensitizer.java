ff45aa4893bf104c3448f5bd3363482141bf762c
周涛 <zhoutaoo@foxmail.com>
2023-07-05 22:56:56 +0800

    新增nacos project.name的配置，取spring.application.name，该值为nacos订阅者应用名

--- opensabre-starter-boot/src/main/java/io/github/opensabre/boot/sensitive/log/desensitizer/LogBackDesensitizer.java
+++ opensabre-starter-boot/src/main/java/io/github/opensabre/boot/sensitive/log/desensitizer/LogBackDesensitizer.java
@@ -8,11 +8,18 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
  * @author zhoutaoo
  */
 public interface LogBackDesensitizer {
+    /**
+     * 是否支持脱敏
+     *
+     * @param event 日志事件
+     * @return true/false 支持/不支持
+     */
+    boolean support(ILoggingEvent event);
 
     /**
      * 脱敏接口定义
      *
-     * @param event  事件
+     * @param event 事件
      */
     String desensitize(final ILoggingEvent event);
 }
