dfa17011f08ce5e14b21d317ee3abe57a88a5040
周涛 <zhoutaoo@foxmail.com>
2023-08-10 21:30:37 +0800

    脱敏规则可通过配置调整

--- opensabre-starter-boot/src/test/java/io/github/opensabre/boot/crypt/JasyptTest.java
+++ opensabre-starter-boot/src/test/java/io/github/opensabre/boot/crypt/JasyptTest.java
@@ -1,8 +1,9 @@
 package io.github.opensabre.boot.crypt;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JasyptTest {
 
@@ -10,16 +11,10 @@ public class JasyptTest {
     public void jasyptTest() {
         //加密工具
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        //加密配置
-        EnvironmentStringPBEConfig config = new EnvironmentStringPBEConfig();
-        //算法类型
-        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         //生成秘钥的公钥
-        config.setPassword("Pa55w0rd@opensabre");
-        //应用配置
-        encryptor.setConfig(config);
+        encryptor.setPassword("Pa55w0rd@opensabre");
         //加密
-        String ciphertext = encryptor.decrypt("3iIE/7wbUTBB0g9WuUHIFbduqiLHafNVnB+Oo+SP6uFGntypXzyZsd1SPlwJchgv");
-        System.out.println(ciphertext);
+        String ciphertext = encryptor.decrypt("yLMBDZMhsJziMcceIT1IWw==");
+        assertEquals("123456", ciphertext);
     }
 }
