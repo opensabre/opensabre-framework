5f1c4177582a989642f7f15e9ebf560c0d0ff98f
周涛 <zhoutaoo@foxmail.com>
2025-01-09 23:11:38 +0800

    knife4j 增加 Bearer Authorization鉴权参数

--- opensabre-starter-boot/src/main/java/io/github/opensabre/boot/config/OpensabreSwaggerConfig.java
+++ opensabre-starter-boot/src/main/java/io/github/opensabre/boot/config/OpensabreSwaggerConfig.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
