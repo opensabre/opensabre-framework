9e27984ccea217f55fd045462bdb898d4209f6f4
周涛 <zhoutaoo@foxmail.com>
2026-08-01 16:17:17 +0800

    feat: 增加 Nacos 公共配置入口

--- opensabre-starter-config/src/main/java/io/github/opensabre/config/OpensabreConfig.java
+++ opensabre-starter-config/src/main/java/io/github/opensabre/config/OpensabreConfig.java
@@ -4,6 +4,9 @@ import io.github.opensabre.boot.config.YamlPropertyLoaderFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * 加载 OpenSabre Nacos 配置中心的共享配置定位规则。
+ */
 @AutoConfiguration
 @PropertySource(value = {"classpath:opensabre-config.yml"}, encoding = "UTF8", factory = YamlPropertyLoaderFactory.class)
 public class OpensabreConfig {
