46fc69ae74a4942bf2a1fcd6dc30821813da9d7d
周涛 <zhoutaoo@foxmail.com>
2026-08-20 23:52:06 +0800

    优化 RPC starter 的 Sentinel 可选依赖

--- opensabre-starter-rpc/src/main/java/io/github/opensabre/rpc/sentinel/config/OpensabreSentinelConfig.java
+++ opensabre-starter-rpc/src/main/java/io/github/opensabre/rpc/sentinel/config/OpensabreSentinelConfig.java
@@ -3,9 +3,11 @@ package io.github.opensabre.rpc.sentinel.config;
 import io.github.opensabre.rpc.sentinel.exception.SentinelExceptionHandlerAdvice;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
+@ConditionalOnClass(name = "com.alibaba.csp.sentinel.slots.block.BlockException")
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Import({SentinelExceptionHandlerAdvice.class})
 public class OpensabreSentinelConfig {
